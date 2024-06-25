6ef4da2a64cfd839669c5f2a803eec6404293029
TecnnoBot03 <simoncarreno2006@gmail.com>
2024-06-24 22:13:06 -0500

    creado el sistema de js para la toggle list de la cuarta seccion, con sus respectivas clases y estilos basicos

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+const toggle1 = document.getElementById('education')
+const toggle2 = document.getElementById('educationContent')
+const toggle3 = document.getElementById('events')
+const toggle4 = document.getElementById('eventsContent')
+const toggle5 = document.getElementById('certifications')
+const toggle6 = document.getElementById('certificationsContent')
+
+
 document.addEventListener("DOMContentLoaded", function() {
     const items = document.querySelectorAll('.carousel-item');
     let currentItem = 0;
     const intervalTime = 3000;
     let autoSlide = setInterval(showNextItem, intervalTime);
+
+    toggle2.classList.toggle('hide')
+    toggle4.classList.toggle('hide')
+    toggle6.classList.toggle('hide')
 
     function showNextItem() {
         items[currentItem].classList.remove('active');
@@ -28,3 +40,15 @@ document.addEventListener("DOMContentLoaded", function() {
         autoSlide = setInterval(showNextItem, intervalTime);
     });
 });
+
+toggle1.addEventListener("click", ()=> {
+    toggle2.classList.toggle('hide')
+})
+
+toggle3.addEventListener("click", ()=> {
+    toggle4.classList.toggle('hide')
+})
+
+toggle5.addEventListener("click", ()=> {
+    toggle6.classList.toggle('hide')
+})
